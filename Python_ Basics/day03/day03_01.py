bfe3b794d82719e10cf7f720e28f39434e4f91de
黄敏争 <mzhuang@storemax.cn>
2018-01-29 20:43:10 +0800

    format code

---
+++ Python_ Basics/day03/day03_01.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+
+# print("hello hmz")
+#
+# age = 18
+# print("age是%d" % age)
+#
+# name = "hmz"
+# print("name是%s" % name)
+
+name = input("请输入名字：")
+
+print("============")
+print("姓名：%s" % name)
+print("============")
