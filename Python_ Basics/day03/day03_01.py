16a80e152a835c0bb64a8844914091fb26165cda
黄敏争 <mzhuang@storemax.cn>
2018-02-07 15:12:17 +0800

    format code

--- Python_ Basics/day03/day03_01.py
+++ Python_ Basics/day03/day03_01.py
@@ -9,8 +9,22 @@
 # name = "hmz"
 # print("name是%s" % name)
 
-name = input("请输入名字：")
 
-print("============")
-print("姓名：%s" % name)
-print("============")
+# name = input("请输入名字：")
+#
+# print("============")
+# print("姓名：%s" % name)
+#
+# print("============")
+
+"""
+a = 2h
+b = 5
+c = a ** 3
+print("a**3是%d" % c)
+"""
+
+c = "Hmz" * 10
+print("c是%s" % c)
+
+s = len("abc")
